3b7f80e5bd8da2c15ef8afb87ed9383b5f5811c8
Qomar034 <nurqomarudin034@gmail.com>
2023-02-28 17:59:03 +0700

    Refactoring Code

--- algoritma/soal2.ts
+++ algoritma/soal2.ts
@@ -8,22 +8,25 @@ const sentence = "Saya sangat senang mengerjakan soal algoritma"
 longest(sentence) 
 // mengerjakan: 11 character */
 
-const testSentence1: string = "Saya sangat senang mengerjakan soal algoritma"
+const testSentence1: string = "Saya sangat senang mengerjakan soal algoritma";
 
 const findLongestCount = (sentence: string): string => {
-    let word: string = "", temporary: string = ""
-    
-    sentence.split("").forEach((el: string, i: number) => {
-        if (el == " " || i == sentence.length) {
-            if (temporary.length > word.length) word = temporary
+  let word: string = "",
+    temporary: string = "";
 
-            temporary = ""
-        } else {
-            temporary += el
-        }
-    })
+  sentence
+    .split("")
+    .forEach((el: string, i: number) => {
+      if (el == " " || i == sentence.length) {
+        if (temporary.length > word.length) word = temporary;
 
-    return `${word}: ${word.length} character`
-}
+        temporary = "";
+      } else {
+        temporary += el;
+      }
+    });
+
+  return `${word}: ${word.length} character`;
+};
 
 console.log(findLongestCount(testSentence1));
