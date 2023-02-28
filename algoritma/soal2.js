782c68e1f6d8231f67657c26294157429899436e
Qomar034 <nurqomarudin034@gmail.com>
2023-02-28 15:37:31 +0700

    Soal 2 Clear

---
+++ algoritma/soal2.js
@@ -0,0 +1,29 @@
+/* 2. Diberikan contoh sebuah kalimat, silahkan cari kata terpanjang dari kalimat tersebut, 
+jika ada kata dengan panjang yang sama silahkan ambil salah satu
+
+Contoh:  
+```
+const sentence = "Saya sangat senang mengerjakan soal algoritma"
+
+longest(sentence) 
+// mengerjakan: 11 character */
+
+const test = "Saya sangat senang mengerjakan soal algoritma"
+
+const findLongestCount = (sentence) => {
+    let word = "", temporary = ""
+    
+    sentence.split("").forEach((el, i) => {
+        if (el == " " || i == sentence.length) {
+            if (temporary.length > word.length) word = temporary
+
+            temporary = ""
+        } else {
+            temporary += el
+        }
+    })
+
+    return `${word}: ${word.length} character`
+}
+
+console.log(findLongestCount(test));
